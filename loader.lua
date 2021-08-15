68fbe841113d5537cd57826588548c95648f3369
fortniteego <77537075+fortniteego@users.noreply.github.com>
2021-08-15 17:07:09 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 1962086868 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/fortniteego/kom-hub/main/TOH.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fortniteego/kom-hub/main/TOH.lua"))()
 elseif game.PlaceId == 3956818381 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/fortniteego/kom-hub/main/NinjaLegends.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fortniteego/kom-hub/main/NinjaLegends.lua"))()
 end
